615bd756287b9da88f2449d016b1d719c40c3f0e
Sonor7 <ghsonor@gmail.com>
2017-07-06 13:32:38 +0200

    V1
    Version 1

---
+++ Windows Update Reset.ps1
@@ -0,0 +1,14 @@
+﻿net stop bits
+net stop cryptsvc
+net stop wuauserv
+#Stop Windows update services, so we can remove the files
+Remove-Item -Recurse C:\windows\SoftwareDistribution\*
+#remove everything under the software distribution folder
+net start bits
+net start cryptsvc
+net start wuauserv
+#start the services, so we'll be able to check for updates after we are done
+Get-AppXPackage | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+#re-register windows stuff. Works miracles
+Read-Host
+#hit enter when you are done
